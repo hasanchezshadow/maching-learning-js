e723dec32f981c1d578423d72570a7bf8c33875c
ShadowWalker <sanchezhernan3@gmail.com>
2024-05-05 22:29:18 +0300

    Initial commit Installing dependencies Testing text-to-speech model

---
+++ index.js
@@ -0,0 +1,21 @@
+import {pipeline} from "@xenova/transformers";
+import wavefile from "wavefile";
+import * as fs from "fs";
+
+const EMBED = 'https://huggingface.co/datasets/Xenova/transformers.js-docs/resolve/main/speaker_embeddings.bin';
+const PHRASE = 'Hello Hernan, how are you? I hope you are doing well today!';
+
+
+const synthesizer = await pipeline(
+    'text-to-speech',
+    'Xenova/speecht5_tts',
+    {quantized: false}
+);
+
+const out = await synthesizer(PHRASE, {speaker_embeddings: EMBED});
+
+const wav = new wavefile.WaveFile();
+wav.fromScratch(1, out.sampling_rate, '32f', out.audio);
+fs.writeFileSync('out.wav', wav.toBuffer());
+
+
